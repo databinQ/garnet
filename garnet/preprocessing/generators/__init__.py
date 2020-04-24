20f5170ecfd517604413f326cda2c77c37e5aa7d
databinQ <blessbingo@163.com>
2020-04-24 08:55:52 +0800

    add data generator

--- garnet/preprocessing/generators/__init__.py
+++ garnet/preprocessing/generators/__init__.py
@@ -4,3 +4,22 @@
 @Author : garnet
 @Time   : 2020/3/3 17:49
 """
+
+import keras
+import typing
+
+from ..packs import DataPack
+
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, data_pack: DataPack, batch_size: int, shuffle: bool = True):
+        ...
+
+    def __len__(self):
+        ...
+
+    def __getitem__(self, item):
+        ...
+
+    def on_epoch_end(self):
+        ...
