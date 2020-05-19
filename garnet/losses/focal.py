0f5723cf756ed0863769c8bb569ca01df84a28cf
databinQ <blessbingo@163.com>
2020-05-19 22:35:43 +0800

    feature(focal loss): add multi categorical focal loss

--- garnet/losses/focal.py
+++ garnet/losses/focal.py
@@ -35,9 +35,64 @@ def binary_focal_loss(gamma=2., alpha=.25):
         pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
         pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
 
-        loss_1 = K.mean(alpha * K.pow((1. - pt_1), alpha) * K.log(pt_1))
-        loss_0 = K.mean((1 - alpha) * K.pow(pt_0, alpha) * K.log(1. - pt_0))
+        loss_1 = K.mean(alpha * K.pow((1. - pt_1), gamma) * K.log(pt_1))
+        loss_0 = K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
         return -loss_1 - loss_0
 
     return binary_focal_loss_fixed
+
+
+def categorical_focal_loss(gamma=2., alpha=.25):
+    """
+    Softmax version of focal loss.
+           m
+      FL = ∑  -alpha * (1 - p_o,c)^gamma * y_o,c * log(p_o,c)
+          c=1
+      where m = number of classes, c = class and o = observation
+    Parameters:
+      alpha -- the same as weighing factor in balanced cross entropy
+      gamma -- focusing parameter for modulating factor (1-p)
+    Default value:
+      gamma -- 2.0 as mentioned in the paper
+      alpha -- 0.25 as mentioned in the paper
+    References:
+        Official paper: https://arxiv.org/pdf/1708.02002.pdf
+        https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
+    Usage:
+     model.compile(loss=[categorical_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+
+    def categorical_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred: A tensor resulting from a softmax
+        :return: Output tensor.
+        """
+
+        if isinstance(alpha, (int, float)):
+            _alpha = alpha
+        else:
+            _shape = K.int_shape(y_pred)
+            assert len(alpha) == _shape[-1]
+            _alpha = K.constant(alpha, dtype=K.dtype(y_pred))
+            for i in range(len(_shape) - 1):
+                _alpha = K.expand_dims(_alpha, axis=0)
+
+        # Scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+
+        # Clip the prediction value to prevent NaN's and Inf's
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+
+        # Calculate Cross Entropy
+        cross_entropy = -y_true * K.log(y_pred)
+
+        # Calculate Focal Loss
+        loss = _alpha * K.pow(1 - y_pred, gamma) * cross_entropy
+
+        # Compute mean loss in mini_batch
+        return K.mean(loss, axis=1)
+
+    return categorical_focal_loss_fixed
